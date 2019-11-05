c21f94521d20bc60a398d9c7f5accbcfbc2b9891
Daniel Kwong <dkwon014@ucr.edu>
2019-11-04 18:49:16 -0800

    Add default routing system

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router,
+   Route,
+   Switch,
+   Link,
+   Redirect} from 'react-router-dom'
+
+// Pages
+import MainPage from './pages/index';
+import NotFoundPage from './pages/404';
+
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default App
