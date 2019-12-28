a0fbff29876789f2440ead00df4a96ecc38a7e70
Daniel Kwong <dkwon014@ucr.edu>
2019-12-28 02:18:47 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BioHack_2020/precache-manifest.1f810c0b812d368fd0ead8e65778ba9b.js"
+  "/BioHack_2020/precache-manifest.45f69e648e13d8a20079100180b5b72c.js"
 );
 
 self.addEventListener('message', (event) => {
