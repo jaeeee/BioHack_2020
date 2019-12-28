66dd9428bce61a6d6d036a8fc59e5fa4d83a657b
Johan Guzman <jguzm022@ucr.edu>
2019-12-28 02:00:04 -0800

    Removes routing navigation bar and adds a bootstrap navigation bar

--- src/App.js
+++ src/App.js
@@ -1,73 +1,68 @@
 import React, { Component } from 'react'
 
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+//import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import { BrowserRouter as Router,
    Route,
-   Switch,
-   Link,
-   Redirect} from 'react-router-dom';
+ //  Switch,
+ //  Link,
+ //  Redirect
+  } from 'react-router-dom';
 
-// import Main from './components/main';
+import Main from './components/main';
 // import { Switch, Route} from 'react-router-dom';
 // Pages
 // import MainPage from './pages/index';
 // import NotFoundPage from './pages/404';
 // import UsersPage from './pages/users';
 
-import Main from './components/main';
-// import { Link } from 'react-router-dom'
-import ReactDOM from 'react-dom'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {fab} from '@fortawesome/free-brands-svg-icons'
-import {faBrain, faAllergies, faCar, faSun, faCloud, faCheck, faEnvelope} from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faBrain, faAllergies, faCar, faSun, faCloud, faCheck, faEnvelope)
+//import { Link } from 'react-router-dom'
+//import ReactDOM from 'react-dom'
+import LandingPage from './components/landingpage';
+
 
 export class App extends Component {
   render() {
     return (
-      // return (
-      //   <Router>
-      //     <Switch>
-      //       <Route exact path="/" component={MainPage} />
-      //       <Route exact path="/404" component={NotFoundPage} />
-      //       <Route exact path="/users" component={UsersPage} />
-      //       <Redirect to="/404"/>
-      //     </Switch>
-      //   </Router>
-      // );
-      <div style={{ height: "100vh", position: "relative" }}>
-        <Layout fixedHeader>
-          <Header
-            title={
-              <span>
-                <span style={{ color: "#ddd" }}>BioHack / </span>
-                <strong>2020</strong>
-              </span>
-            }
-          >
-            <Navigation>
-              <Link to="/">Home</Link>
-              <Link to="/aboutme">About</Link>
-              <Link to="/faq">FAQ</Link>
-              <Link to="/projects">Projects</Link>
-              {/* <Link to="/contact">Contact</Link> */}
-            </Navigation>
-          </Header>
-          <Drawer title="Menu">
-            <Navigation>
-              <Link to="/">Home</Link>
-              <Link to="/aboutme">About</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/faq">FAQ</Link>
-              {/* <Link to="/contact">Contact</Link> */}
-            </Navigation>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
-      </div>
+        <Router>
+            <Route exact path="/" component={LandingPage} />
+            {/* <Route exact path="/404" component={NotFoundPage} />
+            <Route exact path="/users" component={UsersPage} />
+            <Redirect to="/404"/> */}
+        </Router>
+      
+      // <div style={{ height: "100vh", position: "relative" }}>
+      //   <Layout fixedHeader>
+      //     <Header
+      //       title={
+      //         <span>
+      //           <span style={{ color: "#ddd" }}>BioHack / </span>
+      //           <strong>2020</strong>
+      //         </span>
+      //       }
+      //     >
+      //       <Navigation>
+      //         <Link to="/">Home</Link>
+      //         <Link to="/aboutme">About</Link>
+      //         <Link to="/faq">FAQ</Link>
+      //         <Link to="/projects">Projects</Link>
+      //         {/* <Link to="/contact">Contact</Link> */}
+      //       </Navigation>
+      //     </Header>
+      //     <Drawer title="Menu">
+      //       <Navigation>
+      //         <Link to="/">Home</Link>
+      //         <Link to="/aboutme">About</Link>
+      //         <Link to="/projects">Projects</Link>
+      //         <Link to="/faq">FAQ</Link>
+      //         {/* <Link to="/contact">Contact</Link> */}
+      //       </Navigation>
+      //     </Drawer>
+      //     <Content>
+      //       <div className="page-content" />
+      //       <Main />
+      //     </Content>
+      //   </Layout>
+      // </div>
     );
   }
 }
