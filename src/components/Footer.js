297b3266555ca5b43328501f226c616506feb0ee
Johan Guzman <jguzm022@ucr.edu>
2019-12-29 19:31:47 -0800

    Moves Contact component to Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Contact from "./Contact";
+
 import {FaEnvelope, FaFacebook, FaInstagram, FaSlack} from 'react-icons/fa'
 
 export class Footer extends Component {
   render() {
     return (
         <div className="footer">
+          <Contact/>
             <p>Built with love by UCR's BioHack Web Development Team.</p>
             </div>
     );
