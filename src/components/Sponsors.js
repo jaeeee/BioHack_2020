ebc938fc48bdcf21e62a20771971904cbedbf94f
rj <rdrsjck@gmail.com>
2019-12-30 20:46:20 -0800

    Added navbar art

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import {FaUserFriends} from 'react-icons/fa';
+import GreenVirus from '../images/Icon_Green.PNG';
 
 export class Sponsors extends Component {
   render() {
     return (
         <div className="section fadeIn" id = "Sponsors">
-          <h1>Sponsors</h1>
+          <h1><FaUserFriends/> Sponsors</h1>
           <div className="row mt-5">
             <div className="col">
             <h2>Sponsor PlaceHolder</h2>
@@ -13,6 +15,9 @@ export class Sponsors extends Component {
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
+            </div>
+            <div className="col">
+            <img src={GreenVirus} className="virus mt-5 ml-5" alt="Thinking Virus"></img>
             </div>
             <div className="col">
             <h2>Sponsor PlaceHolder</h2>
