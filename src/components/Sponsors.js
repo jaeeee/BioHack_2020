841cc613697680f97d66e3beb309bc099223fecd
Daniel Kwong <dkwon014@ucr.edu>
2020-01-08 12:32:15 -0800

    Sponsors header centered

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -5,32 +5,39 @@ import GreenVirus from '../images/Icon_Green.PNG';
 export class Sponsors extends Component {
   render() {
     return (
-        <div className="section fadeIn" id = "Sponsors">
-          <h1><FaUserFriends/> Sponsors</h1>
-          <div className="row mt-5">
-            <div className="col">
+      <div className="section fadeIn" id="Sponsors">
+        <center>
+          <h1>
+            <FaUserFriends /> Sponsors
+          </h1>
+        </center>
+        <div className="row mt-5">
+          <div className="col">
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
-            </div>
-            <div className="col">
-            <img src={GreenVirus} className="virus mt-5 ml-5" alt="Thinking Virus"></img>
-            </div>
-            <div className="col">
+          </div>
+          <div className="col">
+            <img
+              src={GreenVirus}
+              className="virus mt-5 ml-5"
+              alt="Thinking Virus"
+            ></img>
+          </div>
+          <div className="col">
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
             <h2>Sponsor PlaceHolder</h2>
-            </div>
           </div>
         </div>
-
-      );
+      </div>
+    );
   }
 }
 
