d935d4276812752fc5a969b78ac092df008ef4f7
Daniel Kwong <dkwon014@ucr.edu>
2020-02-16 22:17:47 -0800

    hero image background added

--- src/App.js
+++ src/App.js
@@ -17,51 +17,97 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-
+import Image from "./images/Website-Backdrop.png";
+const styles = {
+  paperContainer: {
+    // backgroundColor: '#000000',
+    // backgroundImage: `url(${Image})`,
+    // height: "100%",
+    // backgroundSize: "cover",
+    // width: "88%",
+    // height: "800px"
+  }
+};
 export class App extends Component {
   render() {
+    // console.log(this.props.location.pathname);
+
     return (
-      <HashRouter>
-        <div className="main-wrapper">
+      <div style={styles.paperContainer}>
+        <HashRouter>
+          <div className="main-wrapper">
             <nav className="sticky-top navbar navbar-expand-lg navigation-bar navbar-dark">
-              <button className="navbar-toggler" type="button" data-toggle="dropdown" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="dropdown"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item">
-                    <NavLink class="nav-link" to="/"><span className="nav-bar-option"><FaHome/> Home</span></NavLink>
+                    <NavLink class="nav-link" to="/">
+                      <span className="nav-bar-option">
+                        <FaHome /> Home
+                      </span>
+                    </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/about"><span className="nav-bar-option"><FaDna/> BioHack</span></NavLink>
+                    <NavLink className="nav-link" to="/about">
+                      <span className="nav-bar-option">
+                        <FaDna /> BioHack
+                      </span>
+                    </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/announcements"><span className="nav-bar-option"><MdAnnouncement/> Announcements</span></NavLink>
+                    <NavLink className="nav-link" to="/announcements">
+                      <span className="nav-bar-option">
+                        <MdAnnouncement /> Announcements
+                      </span>
+                    </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/projects"><span className="nav-bar-option"><GiPodium/> Winners</span></NavLink>
+                    <NavLink className="nav-link" to="/projects">
+                      <span className="nav-bar-option">
+                        <GiPodium /> Winners
+                      </span>
+                    </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/FAQs"><span className="nav-bar-option"><FaQuestion/> FAQs</span></NavLink>
+                    <NavLink className="nav-link" to="/FAQs">
+                      <span className="nav-bar-option">
+                        <FaQuestion /> FAQs
+                      </span>
+                    </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="/sponsors"><span className="nav-bar-option"><FaUserFriends/> Sponsors</span></NavLink>
+                    <NavLink className="nav-link" to="/sponsors">
+                      <span className="nav-bar-option">
+                        <FaUserFriends /> Sponsors
+                      </span>
+                    </NavLink>
                   </li>
                 </ul>
               </div>
             </nav>
             <div className="container">
-              <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
-              <Route path="/announcements" component={Announcements}/>
-              <Route path="/projects" component={Projects}/>
-              <Route path="/FAQs" component={FAQ}/>
-              <Route path="/sponsors" component={Sponsors}/>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/announcements" component={Announcements} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/FAQs" component={FAQ} />
+              <Route path="/sponsors" component={Sponsors} />
             </div>
-            <Footer/>
-        </div>
-      </HashRouter>
-      );
+            <Footer />
+          </div>
+        </HashRouter>
+      // </div>
+    );
     }
   }
   export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import Image from '../images/Website-Backdrop.png'
+// import './HomePage.css'
+
+const styles = {
+  paperContainer: {
+    // backgroundColor: '#000000',   
+    backgroundImage: `url(${Image})`,
+    // height: "100%",
+    backgroundSize: "cover",
+    width: "88%",
+    height: "800px"
+  }
+};
+
+
+// componentWillUnmount() {
+//     document.body.style.backgroundImage = null;
+// }
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="hero fadeIn">
-                <h1>WELCOME TO <span style={{color:"#e2eb98"}}>BIOHACK</span>.</h1>
-                <h3>16 MAY 2020</h3>
-            </div>
-        );
-    }
+  componentWillUnmount() {
+      document.body.style.backgroundImage = null;
+      document.body.style.backgroundSize = null;
+  }
+  render() {
+    document.body.style.backgroundImage = `url(${Image})`;
+    document.body.style.backgroundSize = `cover`;
+
+    return (
+      //  <div style={styles.paperContainer}>
+      <div className="hero fadeIn">
+        <h1>
+          WELCOME TO <span style={{ color: "#e2eb98" }}>BIOHACK</span>.
+        </h1>
+        <h3>16 MAY 2020</h3>
+      </div>
+      //   </div>
+    );
+  }
 }
 
 export default Home;
