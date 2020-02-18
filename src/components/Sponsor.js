82877cf82d6355ec6001814764f1a16a8ece50f0
Rahul Nair <rnair12599@gmail.com>
2020-02-18 04:44:25 -0800

    fixed display issue of sponsors on web and added new sponsors

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -3,11 +3,9 @@ import React, { Component } from 'react';
 class Sponsor extends Component {
     render() {
         return (
-            <div>
                 <a href={this.props.link} target="_blank" rel="noopener noreferrer">
-                    <img src={this.props.logo} className="sponsor-logo" alt="sponsor_logo"/>
+                    <img src={this.props.logo} className = "sponsors_logo"/>
                 </a>
-            </div>
         );
     }
 }
--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -13,15 +13,21 @@ import kgiLogo from '../images/sponsorsLogos/kgi_logo.png';
 import wolframAlphaLogo from '../images/sponsorsLogos/wolfram_alpha_logo.png';
 import xyzLogo from '../images/sponsorsLogos/xyz_logo.png';
 import yerbaMateLogo from '../images/sponsorsLogos/yerba_mate_logo.png';
+import linodeLogo from '../images/sponsorsLogos/linode_logo.png';
+import asucrLogo from '../images/sponsorsLogos/asucr_logo.png';
+import aspbLogo from '../images/sponsorsLogos/aspb_logo.png';
+import mimiscafeLogo from '../images/sponsorsLogos/mimiscafe_logo.png';
+import sketchLogo from '../images/sponsorsLogos/sketch_logo.png';
+
 // import  from '../images/sponsorsLogos';
 
 const sponsorsLeft = [
   { logo: googleLogo , link: "https://www.google.com/" },
   { logo: amgenLogo, link: "https://www.amgen.com/"},
   { logo: cseLogo, link: "https://www1.cs.ucr.edu/"},
-  // Confirm Logo{ logo: "oop", link: "https://www.ootpdevelopments.com/"},
   { logo: kgiLogo, link: "https://www.kgi.edu/"},
-  { logo: bmesLogo, link: "https://bmesucr.weebly.com/"}
+  { logo: bmesLogo, link: "https://bmesucr.weebly.com/"},
+  { logo: linodeLogo, link: "https://www.linode.com/"},
 ]
 
 const sponsorsRight = [
@@ -30,6 +36,17 @@ const sponsorsRight = [
   { logo: xyzLogo, link: "https://nic.xyz/"},
   { logo: digitalOceanLogo, link: "https://www.digitalocean.com/products/droplets/?_campaign=DO_Dev_Awareness_G_Search_B_Generic_Alpha&_adgroup=digitalocean&_keyword=digitalocean&_device=c&_copytype=&_adposition=&_medium=brand_sem&_source=google&_dkitrig=&_2dkitrig=&gclid=CjwKCAiAx_DwBRAfEiwA3vwZYu739GJuAgP07NYQoZ4a54h2ARW_8tzFlhkBixgCiqAPYnQEUd3owBoCAtwQAvD_BwE"},
   { logo: yerbaMateLogo, link: "https://guayaki.com/yerba-mate/"}
+]
+
+const sponsorsTop = [
+  { logo: asucrLogo, link: "https://asucr.ucr.edu/" },
+  { logo: mimiscafeLogo, link: "https://www.mimiscafe.com/"},
+
+]
+
+const sponsorsBottom = [
+  { logo: sketchLogo, link: "https://www.sketch.com/" },
+  { logo: aspbLogo, link: "https://aspb.ucr.edu/"},
 ]
 
 export class Sponsors extends Component {
@@ -50,10 +67,19 @@ export class Sponsors extends Component {
                   link = {sponsor.link}
                   key = {index}
                 />
-              )) 
+              ))
             }
           </div>
           <div className="col">
+            {
+              sponsorsTop.map((sponsor, index) => (
+                <Sponsor
+                  logo = {sponsor.logo}
+                  link = {sponsor.link}
+                  key = {index}
+                />
+              ))
+            }
             <img
               src={GreenVirus1}
               className="happy-virus"
@@ -64,6 +90,15 @@ export class Sponsors extends Component {
               className="happy-virus"
               alt="Happy Virus"
             ></img>
+            {
+              sponsorsBottom.map((sponsor, index) => (
+                <Sponsor
+                  logo = {sponsor.logo}
+                  link = {sponsor.link}
+                  key = {index}
+                />
+              ))
+            }
           </div>
           <div className="col">
             {
@@ -73,7 +108,7 @@ export class Sponsors extends Component {
                   link = {sponsor.link}
                   key = {index}
                 />
-              )) 
+              ))
             }
           </div>
         </div>
