cfe14dc6d5212b9bc88d292843f3b9a462c0a80e
Daniel Kwong <dkwon014@ucr.edu>
2020-02-24 00:08:53 -0800

    Adds announcement for registration

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -5,24 +5,46 @@ class Announcements extends Component {
     render() {
         return (
           <div className="section fadeIn">
-          <h1><MdAnnouncement/> Announcements</h1>
+            <h1>
+              <MdAnnouncement /> Announcements
+            </h1>
             <div className="content" id="Announcements">
-                  <img src={CheeringVirus} className="virus" alt="Cheering Virus"/>
-                  <p>
-                  <ul className="announcement-list">
-                  <li>Priority registration opens on <strong>Monday, February 24th</strong>! Follow us on social media to be notified as soon as applications drop!</li>
-                  <li>Want to <strong>help plan/organize BioHack 2020</strong>? Come to our planning committee meetings!</li>
+              <img src={CheeringVirus} className="virus" alt="Cheering Virus" />
+              <p>
+                <ul className="announcement-list">
+                  <li>
+                    Priority registration for BioHack 2020 is now{" "}
+                    <strong>open</strong>! The deadline for priority
+                    registration is <strong>Friday, April 10th</strong>, so be
+                    sure to sign up now!
+                  </li>
+                  {/* <br> */}
+                  <li>
+                    Priority registration opens on{" "}
+                    <strong>Monday, February 24th</strong>! Follow us on social
+                    media to be notified as soon as applications drop!
+                  </li>
+                  <li>
+                    Want to <strong>help plan/organize BioHack 2020</strong>?
+                    Come to our planning committee meetings!
+                  </li>
                   <br />
-                  <strong>Operations: Tuesdays 10-11 a.m. in Bourns A171</strong>
+                  <strong>
+                    Operations: Tuesdays 10-11 a.m. in Bourns A171
+                  </strong>
                   <br />
                   <strong>Marketing: Wednesdays 4-5 p.m. in WCH 203</strong>
-                  < br />
                   <br />
-                  <li>Feel free to email <strong>biohack.ucr@gmail.com</strong> or contact Tammi Tang/John Shin on social media for further questions/inquiries!</li>
-                  </ul>
-                  </p>
-                </div>
+                  <br />
+                  <li>
+                    Feel free to email <strong>biohack.ucr@gmail.com</strong> or
+                    contact Tammi Tang/John Shin on social media for further
+                    questions/inquiries!
+                  </li>
+                </ul>
+              </p>
             </div>
+          </div>
         );
     }
 }
