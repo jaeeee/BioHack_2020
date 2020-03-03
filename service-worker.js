dc6d99472d3a80bcbf96a65e0c4c34e61a3853fa
Daniel Kwong <dkwon014@ucr.edu>
2020-03-03 00:52:53 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65ffbe97b06393471e4000e789dcdc31.js"
+  "/precache-manifest.dd4a9157405fd2e4f0da64dd97da0876.js"
 );
 
 self.addEventListener('message', (event) => {
